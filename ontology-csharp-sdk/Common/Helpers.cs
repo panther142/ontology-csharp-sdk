dc6b5dd197dce64f78e4339165e6ce3a78871eb0
panther142@protonmail.com <panther142@protonmail.com>
2018-06-03 13:42:38 +1000

    Remove redundant method in Helpers class

--- ontology-csharp-sdk/Common/Helpers.cs
+++ ontology-csharp-sdk/Common/Helpers.cs
@@ -1,36 +1,6 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-
-namespace OntologyCSharpSDK.Common
+﻿namespace OntologyCSharpSDK.Common
 {
     public static class Helpers
     {
-
-        /// <summary>
-        /// Builds a JSON string used to query an RPC node
-        /// </summary>
-        /// <param name="method"></param>
-        /// <param name="param"></param>
-        /// <returns></returns>
-
-        public static string jsonRequestBuilder(string method, IList<object> param)
-        {
-
-            JObject jsonObject = new JObject();
-
-            jsonObject["jsonrpc"] = "2.0";
-            jsonObject["method"] = method;
-            jsonObject["id"] = 1;
-
-            JArray jsonArray = new JArray(param);
-
-            jsonObject.Add("params", jsonArray);
-
-            string json = JsonConvert.SerializeObject(jsonObject);
-            return json;
-
-        }
-
     }
-    }
+}
